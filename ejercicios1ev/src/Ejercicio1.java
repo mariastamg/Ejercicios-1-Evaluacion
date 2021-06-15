774e2d897460e81c9f44165a2524e1c3a8dbe76f
mariastamg <80354648+mariastamg@users.noreply.github.com>
2021-06-15 08:08:24 +0200

    Ejercicio 1 de la unidad 4 resuelto

---
+++ ejercicios1ev/src/Ejercicio1.java
@@ -0,0 +1,40 @@
+package unidad4;
+/*Programa que lea una cadena desde el teclado 
+y muestre en la consola el número de veces que se repite cada vocal.*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+        String texto;
+        char caracter;
+        int numeroDeVeces = 0;
+        do {
+            System.out.println("Introduce texto: ");
+            texto = sc.nextLine();
+        } while (texto.isEmpty());
+        System.out.print("Introduce un carácter: ");
+        caracter = (char) System.in.read();
+        numeroDeVeces = contarCaracteres(texto, caracter);
+        System.out.println("El caracter " + caracter + " aparece " + numeroDeVeces + " veces");                   
+    }
+	
+	public static int contarCaracteres(String cadena, char caracter) {
+        int posicion, contador = 0;
+        //se busca la primera vez que aparece
+        posicion = cadena.indexOf(caracter);
+        while (posicion != -1) { //mientras se encuentre el caracter
+            contador++;           //se cuenta
+            //se sigue buscando a partir de la posición siguiente a la encontrada
+            posicion = cadena.indexOf(caracter, posicion + 1);
+        }
+        return contador;
+
+
+	}
+
+
+}
